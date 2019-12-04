c5ea07bcd4c6ac51fecd2dad979471d327009dbe
ZongMinFeng <2329706697@qq.com>
2019-12-04 17:06:04 +0800

    v1.0.0 修改

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '光武会员系统',
 
   /**
    * @type {boolean} true | false
--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
-'use strict'
-const path = require('path')
-const defaultSettings = require('./src/settings.js')
+'use strict';
+const path = require('path');
+const defaultSettings = require('./src/settings.js');
 
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'vue Admin Template' // page title
+const name = defaultSettings.title || 'Vue Admin Template'; // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
 // For example, Mac: sudo npm run
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
-const port = process.env.port || process.env.npm_config_port || 9528 // dev port
+const port = process.env.port || process.env.npm_config_port || 9528; // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -53,7 +53,7 @@ module.exports = {
     proxy: {
       '/': {
         ws: false,             //相关代理端不支持 ws
-        target: 'http://127.0.0.1:8081',//本地
+        target: 'http://127.0.0.1:18081',//本地
         secure: false,
         changeOrigin: true
       },
